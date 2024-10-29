ffeb2998a5935baefba6466fa604836b9fd2091f
hamday_ <ilhamhudaya29@gmail.com>
2024-10-29 16:17:29 +0700

    asd

---
+++ task3.py
@@ -0,0 +1,18 @@
+
+
+provinsi = {
+    'Nanggroe Aceh Darussalam': 'Aceh',
+    'Sumatera Selatan': 'Palembang',
+    'Kalimantan Barat': 'Pontianak',
+    'Jawa Timur': 'Madiun',
+    'Sulawesi Selatan': 'Makassar',
+    'Maluku': 'Ambon'
+}
+
+listprovinsi = list(provinsi.keys())
+print(listprovinsi)  
+
+
+provinsi['Jawa Timur'] = 'Surabaya'
+print(provinsi)
+
